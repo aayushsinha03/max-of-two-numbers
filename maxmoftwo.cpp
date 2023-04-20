3f689f74a3598e383af8cbdbc6937e079ceb7327
Aayush sinha <130181905+aayushsinha03@users.noreply.github.com>
2023-04-21 01:49:12 +0530

    Add files via upload

---
+++ maxmoftwo.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    int y;
+    cout<<"enter the value of x"<<endl;
+    cin>>x;
+    cout<<"enter the value of y"<<endl;
+    cin>>y;
+    if(x>y)
+    {
+            cout<<"x is greater"<<endl;
+    else
+            cout<<"y is greater"<<endl;
+    }
+    return 0;
+}
